6a93f2becc32b914cc238f4e3e573c78be80d07e
Mario Solomon <solomon.mario@gmail.com>
2013-10-29 17:01:23 -0700

    Users and Addition

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
+
+
+
+@app.route("/hello")
 def hello ():
-	return "hello world"
+    return "hello world"
+
+@app.route("/")
+def index ():
+    return "Index"
+
+@app.route("/user/<username>")
+def user(username):
+    #print 'user: ' + username
+    lower=username.lower()
+    upper=username.upper()
+    both = lower + " " + upper
+    return both
+
+@app.route("/add/<int:p>/<int:m>")
+def integer_adder (p,m):
+    addition = p+m
+    return "sum of " + str(addition)
+
+
+
 if __name__ == "__main__":
+    app.debug=True
     app.run()
